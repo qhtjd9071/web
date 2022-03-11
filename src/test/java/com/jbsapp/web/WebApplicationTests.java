a9200359de63e9c13598a27dbc27839f6119f169
jbs <qhtjd9071@naver.com>
2022-03-11 23:43:01 +0900

    초기 세팅

---
+++ src/test/java/com/jbsapp/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbsapp.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
