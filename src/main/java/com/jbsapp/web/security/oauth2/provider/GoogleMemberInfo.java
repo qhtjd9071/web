cb898b6322dc16069d7ed797ba8e5da980c0ba68
qhtjd9071 <qhtjd9071@naver.com>
2022-03-17 13:55:12 +0900

    로그인 - OAuth 2.0 구글 로그인 추가

---
+++ src/main/java/com/jbsapp/web/security/oauth2/provider/GoogleMemberInfo.java
@@ -0,0 +1,32 @@
+package com.jbsapp.web.security.oauth2.provider;
+
+import java.util.Map;
+
+public class GoogleMemberInfo implements OAuth2UserInfo {
+
+        private final Map<String, Object> attributes;
+
+        public GoogleMemberInfo(Map<String, Object> attributes) {
+                this.attributes = attributes;
+        }
+
+        @Override
+        public String getProviderId() {
+                return attributes.get("sub").toString();
+        }
+
+        @Override
+        public String getProvider() {
+                return "google";
+        }
+
+        @Override
+        public String getName() {
+                return attributes.get("name").toString();
+        }
+
+        @Override
+        public String getEmail() {
+                return attributes.get("email").toString();
+        }
+}
